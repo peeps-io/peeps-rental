61fee32cffcb51cfb7c119e7a86dda777de54472
peeps-io <peeps.io.inc@gmail.com>
2025-12-02 02:54:10 -0800

    added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import Faq from "@/sections/Faq";
 import Navbar from "../components/pages/navbar";
 import CTA from "@/sections/Cta";
 import Footer from "@/components/pages/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center">
+      <Analytics />
       <Navbar />
       <Hero />
       <Features />
